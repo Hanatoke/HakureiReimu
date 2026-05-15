efbf08168b8e4953de0aa0b48748d2a5576d7acc
诗音 <1091473582@qq.com>
2026-05-15 14:09:47 +0800

    update

--- HakureiReimuMod/Powers/SoraTobuFushigiNoMikoPower.cs
+++ HakureiReimuMod/Powers/SoraTobuFushigiNoMikoPower.cs
@@ -1,8 +1,12 @@
-﻿using BaseLib.Abstracts;
+﻿using System.Threading.Tasks;
+using BaseLib.Abstracts;
 using BaseLib.Extensions;
 using Godot;
 using HakureiReimu.HakureiReimuMod.Cards.Skill.Uncommon;
 using HakureiReimu.HakureiReimuMod.Extensions;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Powers;
@@ -18,5 +22,19 @@ namespace HakureiReimu.HakureiReimuMod.Powers
             return ResourceLoader.Exists(path) ? path : "power.png".PowerImagePath();
         }}
         public string CustomPackedIconPath => CustomBigIconPath;
+
+        public override Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override async Task AfterSideTurnStart(CombatSide side, ICombatState combatState)
+        {
+            if (side != Owner.Side)
+                return;
+            Flash();
+            await PowerCmd.Remove(this);
+            await PowerCmd.Apply<DexterityPower>(new BlockingPlayerChoiceContext(), Owner, -Amount, Owner, null);
+        }
     }
 }
